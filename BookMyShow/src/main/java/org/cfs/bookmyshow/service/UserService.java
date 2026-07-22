1f2dd2049130abb60907fb611d6ffb37c5a12ad3
io303 <srahul12768@gmail.com>
2026-07-22 17:04:23 +0530

    Services added

---
+++ BookMyShow/src/main/java/org/cfs/bookmyshow/service/UserService.java
@@ -0,0 +1,63 @@
+package org.cfs.bookmyshow.service;
+
+import org.cfs.bookmyshow.dto.UserDto;
+import org.cfs.bookmyshow.exception.ResourceNotFoundException;
+import org.cfs.bookmyshow.model.User;
+import org.cfs.bookmyshow.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private UserDto createUser(UserDto userDto)
+    {
+        User user=mapToEntity(userDto);
+        User savedUser=userRepository.save(user);
+        return mapToDto(savedUser);
+    }
+
+    public UserDto getUserById(Long id)
+    {
+        User user=userRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Use not found with id: "+id));
+        return mapToDto(user);
+    }
+
+    public List<UserDto> getAllUsers()
+    {
+        List<User> users=userRepository.findAll();
+        return users.stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    //update user
+    //delete user
+
+    private User mapToEntity(UserDto userDto) {
+        User user=new User();
+        user.setEmail(userDto.getEmail());
+        user.setName(userDto.getName());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+        return  user;
+    }
+
+    private UserDto mapToDto(User user)
+    {
+        UserDto userDto=new UserDto();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+        userDto.setPhoneNumber(user.getPhoneNumber());
+        return userDto;
+
+    }
+}
+
